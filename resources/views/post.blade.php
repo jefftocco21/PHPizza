44c29dcccc342c56636447f990333089dabfd791
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-02-23 10:08:46 -0500

    creating layout file in components

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,29 +1,12 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends ('layout')
 
-        <title>PHPizza</title>
+@section('content')
+<article class="container mx-auto">
+    <h1>{!!$post->title!!}</h1>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-        <!-- Styles -->
-        <link href="/app.css" rel="stylesheet">
-        <link rel="stylesheet" href="public/app.css">
-    </head>
-    <body>
-        <article class="container mx-auto">
-            <h1>{!!$post->title!!}</h1>
-
-            <div>
-                {!! $post->body; !!}
-            </div>
-        </article>
-
-        <a href="/">Go Back</a>
-    </body>
-</html>
+    <div>
+        {!! $post->body; !!}
+    </div>
+</article>
+<a href="/">Go Back</a>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,32 +1,14 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>PHPizza</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-        <!-- Styles -->
-        <link href="/app.css" rel="stylesheet">
-        <link rel="stylesheet" href="public/app.css">
-    </head>
-    <body>
-        @foreach ($posts as $post)
-            <article>
-                <h1>
-                    <a href="/posts/{{$post->slug}}">
-                        {{ $post->title }}
-                    </a>
-                </h1>
-                <div>
-                    {!!$post->body!!}
-                </div>
-            </article>
-        @endforeach
-    </body>
-</html>
+<x-layout>
+    @foreach ($posts as $post)
+    <article>
+        <h1>
+            <a href="/posts/{{$post->slug}}">
+                {{ $post->title }}
+            </a>
+        </h1>
+        <div>
+            {!!$post->body!!}
+        </div>
+    </article>
+    @endforeach
+</x-layout>

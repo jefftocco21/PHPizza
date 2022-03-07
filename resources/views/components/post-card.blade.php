9ada9d717f5a089d7848974747e171c4be35393a
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-03-07 14:02:16 -0500

    Creating a dynamic post page and creating a button component

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -9,9 +9,7 @@
         <div class="mt-8 flex flex-col justify-between">
             <header>
                 <div class="space-x-2">
-                    <a href="/toppings/{{$post->topping->slug}}"
-                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                        style="font-size: 10px">{{$post->topping->name}}</a>
+                    <x-topping-button :topping="$post->topping"/>
                 </div>
 
                 <div class="mt-4">
---
+++ resources/views/components/topping-button.blade.php
@@ -0,0 +1,5 @@
+@props(['topping'])
+
+<a href="/toppings/{{$topping->slug}}"
+    class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
+    style="font-size: 10px">{{$topping->name}}</a>

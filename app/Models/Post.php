bce04801c87f4494986ea5d5b39790e037e0b5cd
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-03-09 10:13:33 -0500

    filtering complete

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -22,21 +22,29 @@ class Post extends Model
 
     public function scopeFilter($query, array $filters)
     {
-        $query->when($filters['search'] ?? false, fn($query, $search)=>
-            $query
-                ->where('title', 'like', '%' . $search . '%')->orWhere('body', 'like', '%' . $search . '%'));
+        $query->when($filters['search'] ?? false, fn ($query, $search) =>
+        $query->where(fn ($query) =>
+        $query->where('title', 'like', '%' . $search . '%')
+            ->orWhere('body', 'like', '%' . $search . '%')));
 
-        $query->when($filters['topping'] ?? false, fn($query, $topping)=>
-            $query->whereHas('topping', fn($query)=>
+        $query->when(
+            $filters['topping'] ?? false,
+            fn ($query, $topping) =>
+            $query->whereHas(
+                'topping',
+                fn ($query) =>
                 $query->where('slug', $topping)
             )
         );
 
-        $query->when($filters['author'] ?? false, fn($query, $author)=>
-        $query->whereHas('author', fn($query)=>
-            $query->where('username', $author)
-        )
-    );
+        $query->when(
+            $filters['author'] ?? false,
+            fn ($query, $author) =>
+            $query->whereHas(
+                'author',
+                fn ($query) =>
+                $query->where('username', $author)
+            )
+        );
     }
-
 }

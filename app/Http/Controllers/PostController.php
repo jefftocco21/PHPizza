1e9ed7ef8d60ce5d1fa1a78b97903c8c6c834021
jnt369@gmail.com <jeff.tocco@lyonsexpress.com>
2022-03-09 11:51:55 -0500

    Pagination and publishing vendor files

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,16 +9,16 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts=Post::latest();
+        // $posts=Post::latest();
 
-        if(request('search')){
-            $posts
-                ->where('title', 'like', '%' . request('search') . '%')
-                ->orWhere('body', 'like', '%' . request('search') . '%');
-        }
+        // if(request('search')){
+        //     $posts
+        //         ->where('title', 'like', '%' . request('search') . '%')
+        //         ->orWhere('body', 'like', '%' . request('search') . '%');
+        // }
 
          return view('posts.index',[
-            'posts' => Post::latest()->filter(request(['search', 'topping', 'author']))->get()
+            'posts' => Post::latest()->filter(request(['search', 'topping', 'author']))->paginate(6)->withQueryString()
         ]);
     }
 
--- resources/views/components/topping-dropdown.blade.php
+++ resources/views/components/topping-dropdown.blade.php
@@ -7,11 +7,13 @@
                 <x-arrow-down class="absolute pointer-events-none" />
             </button>
         </x-slot>
-        <x-custom-drop-item href="/" :active="request()->routeIs('home')">All</x-custom-drop-item>
+        <x-custom-drop-item href="/?{{ http_build_query(request()->except('topping', 'page'))}}"
+            :active="request()->routeIs('home')">All
+        </x-custom-drop-item>
 
         <!--bind the display of this div to the truth value of the x-data div -->
         @foreach ($toppings as $topping)
-            <x-custom-drop-item href="/?topping={{ $topping->slug }}&{{ http_build_query(request()->except('topping'))}}"
+            <x-custom-drop-item href="/?topping={{ $topping->slug }}&{{ http_build_query(request()->except('topping', 'page'))}}"
                 :active='request()->is("toppings/{$topping->slug}")'>{{ ucwords($topping->name) }}
             </x-custom-drop-item>
         @endforeach
